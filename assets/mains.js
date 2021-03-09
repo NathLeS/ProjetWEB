c7a7c0635c58d3e7358a97df894eb95e73976567
nath <nath@MacBook-Pro-de-Nathan.local>
2021-03-09 11:08:19 +0100

    2eme structure

--- assets/mains.js
+++ assets/mains.js
@@ -55,3 +55,26 @@ $(function () {
         ],
     });
 });
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navBtn');
+    const navBtn = document.querySelectorAll('.navBtn li');
+    
+    burger.addEventListener('click', ()=> {
+        // Ouverture du menu
+        nav.classList.toggle('navOpen');
+        // Fondu des boutons
+        navBtn.forEach((link, index)=> {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navBtnFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        // Animation burger
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
