125b976a43632d0516293c85e43691c1b47c0542
Elise Beaussart <elise.beaussart@viacesi.fr>
2021-03-25 11:26:34 +0100

    Manager fonction generale du modele

---
+++ Model/Manager.php
@@ -0,0 +1,22 @@
+<?php
+
+class Manager {
+    private $pdo;
+    private $user = 'root';
+    private $pass = 'elise230501';
+    //private $error = '';
+
+    public function __construct(){
+        $this->pdo = new PDO('mysql:host=localhost;dbname=ProjetWEB', $this->user, $this->pass, 
+        [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ          //connexion à la BDD et par défault on récupère sous forme d'objet tous les PDO
+        ]);    
+    }
+
+    public function SELECTALL(){
+        $query = $this->pdo->query("SELECT * FROM etudiant");             //requête SQL  
+        $students = $query->fetchAll();  
+        return $students;
+    }
+
+}
